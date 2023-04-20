3061df5a9076b425de5d5a5737605d722abc4da9
VoMinhTuan21 <vominhtuan.8a1@gmail.com>
2023-04-20 10:43:09 +0700

    feat: update function order revenue

--- src/modules/product/product.service.ts
+++ src/modules/product/product.service.ts
@@ -1718,4 +1718,20 @@ export class ProductService {
     const productItem = await this.productItemModel.findById(id);
     return productItem.salesQuantity;
   }
+
+  async getProductItemIdsByCategoryId(categoryId: string) {
+    const productItemIds: string[] = [];
+
+    const products = await this.productModel
+      .find({ categories: new mongoose.Types.ObjectId(categoryId) })
+      .populate('productItems');
+
+    for (const product of products) {
+      for (const item of product.productItems as ProductItemDocument[]) {
+        productItemIds.push(item._id);
+      }
+    }
+
+    return productItemIds;
+  }
 }
